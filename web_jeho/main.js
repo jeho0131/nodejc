e3778c95804dabff697fdb2ea0c25c38d0628eaa
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-07-22 14:57:22 +0900

    Add files via upload

--- web_jeho/main.js
+++ web_jeho/main.js
@@ -2,6 +2,36 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url')
 
+function templateHTML(title,list,body) {
+  return `
+  <!doctype html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${body}
+    </body>
+  </html>
+  `;
+}
+
+function templateList(filelist) {
+  var list = '<ul>';
+
+  var i = 0;
+  while(i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+
+  return list;
+}
+
 var app = http.createServer(function(request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -9,53 +39,26 @@ var app = http.createServer(function(request, response) {
 
   if(pathname === '/') {
     if(queryData.id === undefined) {
-      var title = 'Welcom';
-      var description = 'Hello, Node.js';
-      var template = `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
-    } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description) {
-        var title = queryData.id;
-        var template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
+      fs.readdir('./data', function(error,filelist) {
+        var title = 'Welcom';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
+
+        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir('./data', function(error,filelist) {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err,description) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
@@ -64,4 +67,4 @@ var app = http.createServer(function(request, response) {
   }
 });
 
-app.listen(3000);
+app.listen(80);
