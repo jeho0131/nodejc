ecfb01b8cf67730fe65a5b7298ffefdc2c999256
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-08-03 12:32:51 +0900

    Add files via upload

--- web_jeho2/main.js
+++ web_jeho2/main.js
@@ -2,7 +2,27 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var host = '192.168.0.35';
+function templateHTML(title) {
+  return `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="index.html">WEB</a></h1>
+      <ul>
+        <li><a href="/?title=Hello">click1</a></li>
+        <li><a href="/?title=My Name Is">click2</a></li>
+        <li><a href="/?title=Jeho">click3</a></li>
+      </ul>
+      <h2> ${title} </h2>
+    </body>
+  </html>
+  `;
+}
+
+var host = '192.168.0.4';
 var port = 3000;
 
 var dooliwebServer = http.createServer(
@@ -10,44 +30,25 @@ var dooliwebServer = http.createServer(
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
 
-    response.writeHead(200);
-    if(queryData.title == undefined) {
-      var template = `
-      <!doctype html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1> MAIN </h1>
-          <ol>
-            <a href="/?title=ABC">click1</a>
-            <a href="/?title=DEF">click2</a>
-            <a href="/?title=GHI">click3</a>
-          </ol>
-        </body>
-      </html>
-      `;
-      response.end(template);
+    if(queryData.title === undefined) {
+      fs.readdir('./data', function(error, filelist) {
+        title = 'MAIN';
+        var list = '<ul>';
+        var i = 0;
+        while(i < filelist.length) {
+          list = list + `<li><a href="?title=${filelist[i]}">click+${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + '</ul>';
+      });
+      title = 'MAIN';
+      var template = templateHTML(title);
     } else {
-      var template = `
-      <!doctype html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1> ${queryData.title} </h1>
-          <ol>
-            <a href="/?title=ABC">click1</a>
-            <a href="/?title=DEF">click2</a>
-            <a href="/?title=GHI">click3</a>
-          </ol>
-        </body>
-      </html>
-      `;
-      response.end(template);
+      title = queryData.title;
+      var template = templateHTML(title);
     }
+    response.writeHead(200);
+    response.end(template);
   }
 );
 
